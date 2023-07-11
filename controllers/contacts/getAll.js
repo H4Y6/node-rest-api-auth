e598ae38ca402a266377bc16ee6b19d34c0bf0fe
Heorhii Chub <h4y6@i.ua>
2023-07-11 17:46:27 +0300

    modified:   controllers/contacts/getAll.js

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -3,9 +3,12 @@ const { Contact } = require(`${basedir}/models/contact`);
 
 const getAll = async (req, res) => {
   const { id: owner } = req.user;
+  const { page = 1, limit = 20, favorite = true } = req.query;
+  const skip = (page - 1) * limit;
   const result = await Contact.find(
-    { owner },
-    "-createdAt -updatedAt"
+    { owner, favorite },
+    "-createdAt -updatedAt",
+    { skip, limit, favorite }
   ).populate("owner", "email -_id");
   res.json(result);
 };
